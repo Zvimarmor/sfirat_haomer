fd412a14e280c7cfe4b9b4da0263eee461962d93
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-04-28 15:11:38 +0300

    sends the right day

--- sfirat_haomer.py
+++ sfirat_haomer.py
@@ -4,7 +4,12 @@ import datetime
 
 sfirot = ["חסד", "גבורה", "תפארת", "נצח", "הוד", "יסוד", "מלכות"]
 sfirots = ["chesed", "gevurah", "tiferet", "netzach", "hod", "yesod", "malchut"]
+lel_haseder_date = datetime.datetime(2024, 4, 23)
 
+def what_day_is_it():
+    now = datetime.datetime.now()
+    return now - lel_haseder_date
+    
 
 def what_sfira_is_it(day):
         if day < 1 or day > 49:
@@ -19,7 +24,7 @@ class sfirat_haomer:
         self.sfira = what_sfira_is_it(day)
     
     def __str__(self) -> str:
-        return get_display(" ספירת העומר היום היא" + str(self.day) + " לעומר, שהיא " + self.sfira[0] + " שב" + self.sfira[1])
+        return get_display("ספירת העומר היום היא " + str(self.day) + " לעומר, שהיא " + self.sfira[0] + " שב" + self.sfira[1])
 
 def return_sfirat_haomer_from_user():
     day = int(input(get_display("הכנס את יום ספירת העומר: ")))
@@ -32,14 +37,18 @@ def return_sfirat_haomer_from_user():
             day = int(input(get_display("הכנס את יום ספירת העומר: ")))
     today = sfirat_haomer(day)
     print(today)
+
+def send_Whatsapp_messege(message):
+    now = datetime.datetime.now()
+    hours = now.hour
+    minutes = now.minute + 1
+    pywhatkit.sendwhatmsg("+972534600460", message, hours, minutes+1)
     
 
 
 if __name__ == "__main__":
-    today = sfirat_haomer(1)
-    #return_sfirat_haomer_from_user()
-    now = datetime.datetime.now()
-    hours = now.hour
-    minutes = now.minute + 1
-    pywhatkit.sendwhatmsg("+972534600460", get_display(str(today)), hours, minutes+1)
+    today = sfirat_haomer(what_day_is_it().days)
+    message = get_display(str(today))
+    send_Whatsapp_messege(message)
+    
 
