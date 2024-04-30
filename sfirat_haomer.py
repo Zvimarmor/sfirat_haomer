a6d4ccb847a739506a5b848d7419b4b7e9f989fd
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-04-30 14:24:51 +0300

    adding the Shkiaa thing

--- sfirat_haomer.py
+++ sfirat_haomer.py
@@ -4,15 +4,24 @@ import datetime
 import time
 
 sfirot = ["חסד", "גבורה", "תפארת", "נצח", "הוד", "יסוד", "מלכות"]
-sfirots = ["chesed", "gevurah", "tiferet", "netzach", "hod", "yesod", "malchut"]
 lel_haseder_date = datetime.datetime(2024, 4, 23)
+
+def get_sunset_time():
+    sunset_time = datetime.datetime.combine(datetime.date.today(), datetime.time(14, 15))
+    return sunset_time
 
 def what_day_is_it():
     now = datetime.datetime.now()
-    answer = now - lel_haseder_date
-    if answer.days < 1:
+    sunset_time = get_sunset_time()
+
+    if now < sunset_time:
+        answer = now - lel_haseder_date
+    else:
+        answer = now - lel_haseder_date + datetime.timedelta(days=1)
+    answer = answer.days
+    if answer < 1:
         return "Invalid day"
-    elif answer.days > 49:
+    elif answer > 49:
         return "Invalid day"
     return answer
     
@@ -25,11 +34,11 @@ def what_sfira_is_it(day):
 
 class sfirat_haomer:
     def __init__(self, day) -> None:
-        self.day = day
+        self.day = int(day)
         self.sfira = what_sfira_is_it(day)
     
     def __str__(self) -> str:
-        return get_display("ספירת העומר היום היא " + str(self.day) + " לעומר, שהיא " + self.sfira[0] + " שב" + self.sfira[1])
+        return get_display("היום " + str(self.day) + " ימים לעומר. הספירה של היום היא " + self.sfira[0] + " שב" + self.sfira[1])
 
 def return_sfirat_haomer_from_user():
     day = int(input(get_display("הכנס את יום ספירת העומר: ")))
@@ -45,7 +54,7 @@ def return_sfirat_haomer_from_user():
 
 def build_message_for_user(today, Nosach = "Sfard"):
     message = get_display("היי! זהו תזכורת לספירת העומר היומית שלך!" + "\n")
-    message += get_display("ברוך אתה ה' אלוהינו מלך העולם, אשר קדשנו במצוותיו וציוונו על ספירת העומר. יום ה" + "\n")
+    message += get_display("ברוך אתה ה' אלוהינו מלך העולם, אשר קדשנו במצוותיו וציוונו על ספירת העומר." + "\n")
     message += str(sfirat_haomer(today)) + "\n"
     return message
 
@@ -60,17 +69,18 @@ def send_Whatsapp_messege(message, phone_number = "+972534600460"):
 def main():
     user_phone_number = input(get_display("מה מספר הפלאפון שלך לשליחת התזכורת( כולל קידומת מדינה)?"))
     user_Nosach = input(get_display("מה נוסח התפילה שלך?"))
-    today = what_day_is_it().days
+    today = what_day_is_it()
     message = build_message_for_user(today, user_Nosach)
     while today <= 49:
-        today = what_day_is_it().days
+        today = what_day_is_it()
         message = build_message_for_user(today, user_Nosach)
-        send_Whatsapp_messege(message, user_phone_number)
-        time.sleep(24*60*60)
+        send_Whatsapp_messege(get_display(message), user_phone_number)
+        time.sleep(10)
+    print(get_display("סיימתי לשלוח תזכורות!"))
+    return
     
 
 if __name__ == "__main__":
-    main()
-    
+    main()   
 
 
