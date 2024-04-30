c5ebbf1ed0dd26bd9c8cf6b0c7bc2a5b2376a2e0
Zvi.marmor@mail.huji.ac.il <Zvi.marmor@mail.huji.ac.il>
2024-04-30 13:39:03 +0300

    almost done and working! besides it is slow and does not have nosachs difference yet..

--- sfirat_haomer.py
+++ sfirat_haomer.py
@@ -1,6 +1,7 @@
 from bidi.algorithm import get_display
 import pywhatkit as pywhatkit
 import datetime
+import time
 
 sfirot = ["חסד", "גבורה", "תפארת", "נצח", "הוד", "יסוד", "מלכות"]
 sfirots = ["chesed", "gevurah", "tiferet", "netzach", "hod", "yesod", "malchut"]
@@ -8,9 +9,13 @@ lel_haseder_date = datetime.datetime(2024, 4, 23)
 
 def what_day_is_it():
     now = datetime.datetime.now()
-    return now - lel_haseder_date
+    answer = now - lel_haseder_date
+    if answer.days < 1:
+        return "Invalid day"
+    elif answer.days > 49:
+        return "Invalid day"
+    return answer
     
-
 def what_sfira_is_it(day):
         if day < 1 or day > 49:
             return "Invalid day"
@@ -38,17 +43,34 @@ def return_sfirat_haomer_from_user():
     today = sfirat_haomer(day)
     print(today)
 
-def send_Whatsapp_messege(message):
+def build_message_for_user(today, Nosach = "Sfard"):
+    message = get_display("היי! זהו תזכורת לספירת העומר היומית שלך!" + "\n")
+    message += get_display("ברוך אתה ה' אלוהינו מלך העולם, אשר קדשנו במצוותיו וציוונו על ספירת העומר. יום ה" + "\n")
+    message += str(sfirat_haomer(today)) + "\n"
+    return message
+
+
+def send_Whatsapp_messege(message, phone_number = "+972534600460"):
     now = datetime.datetime.now()
     hours = now.hour
     minutes = now.minute + 1
-    pywhatkit.sendwhatmsg("+972534600460", message, hours, minutes+1)
-    
+    print("the message will be sent at: " + str(hours) + ":" + str(minutes))
+    pywhatkit.sendwhatmsg(phone_number, message, hours, minutes+1)
 
+def main():
+    user_phone_number = input(get_display("מה מספר הפלאפון שלך לשליחת התזכורת( כולל קידומת מדינה)?"))
+    user_Nosach = input(get_display("מה נוסח התפילה שלך?"))
+    today = what_day_is_it().days
+    message = build_message_for_user(today, user_Nosach)
+    while today <= 49:
+        today = what_day_is_it().days
+        message = build_message_for_user(today, user_Nosach)
+        send_Whatsapp_messege(message, user_phone_number)
+        time.sleep(24*60*60)
+    
 
 if __name__ == "__main__":
-    today = sfirat_haomer(what_day_is_it().days)
-    message = get_display(str(today))
-    send_Whatsapp_messege(message)
+    main()
     
+
 
